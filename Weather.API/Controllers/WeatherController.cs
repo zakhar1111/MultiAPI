7bd0bf1a0cd3943f92c62b88e81f0f4711dec665
Vsevolod Zakharchenko <vzakha@softserveinc.com>
2022-02-23 18:45:57 +0200

    Add project files.

---
+++ Weather.API/Controllers/WeatherController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+using Weather.API.Model;
+
+namespace Weather.API.Controllers
+{
+    [Route("api/[controller]")]   
+    [ApiController]              
+    public class WeatherController : ControllerBase
+    {
+        private IConfigurationRoot ConfigRoot;
+        public WeatherController(IConfiguration configRoot)
+        {
+            ConfigRoot = (IConfigurationRoot)configRoot;
+        }
+
+
+        [HttpGet]                
+        public string Get()
+        {
+            return getWeather(this.DefaultLocation);
+            
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(string id)
+        {
+            return getWeather(id);
+        }
+
+        
+      
+        public string DefaultLocation
+        {
+            get
+            { 
+                string location = ConfigRoot.GetValue<string>("Location:Default");
+                return location;
+            }
+        }
+
+        public string APPID
+        {
+            get
+            {
+                string weatherAppID = ConfigRoot.GetValue<string>("EndPoint:APPID");
+                return weatherAppID;
+            }
+        }
+
+        
+
+        private string AddCityToUri(string city)
+        {
+            return $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APPID}&units=metric&cnt=1";
+        }
+
+        private string getWeather(string city)
+        {
+            var url = AddCityToUri(city); 
+            using (var web = new WebClient())
+            {
+                var json = web.DownloadString(url);
+                var result = JsonConvert.DeserializeObject<WeatherType.root>(json);
+                WeatherType.root outPut = result;
+                return outPut.ToString();
+            }
+        }
+     
+    }
+}
---
+++ Weather.API/Model/WeatherType.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Weather.API.Model
+{
+    public class WeatherType
+    {
+        public class clouds
+        {
+            public string all { get; set; }
+            public override string ToString() =>
+                $"clouds: {all}";
+        }
+
+        public class wind
+        {
+            public double speed { get; set; }
+            public override string ToString() =>
+                $"wind: {this.speed}";
+        }
+        public class main
+        {
+            public double feels_like { get; set; }
+            public double temp { get; set; }
+            public int humidity { get; set; }
+            public int pressure { get; set; }
+            public override string ToString() =>
+               $"temp: {this.temp} feel_like: {this.feels_like} humidity: {this.humidity}  pressure: {this.pressure}";
+        }
+        public class root
+        {
+            public main main { get; set; } //temp
+            public wind wind { get; set; } //speed
+            public clouds clouds { get; set; }//cloud
+            public double dt { get; set; }//date ms
+
+            public string dt_txt { get; set; }
+
+            public override string ToString() =>
+                 $" date= {this.GetDate()}  {main.ToString()}  {wind.ToString()}  {clouds.ToString()}  dt_txt = {this.dt_txt}";
+            public DateTime GetDate ()
+            {
+                var day = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Local).ToLocalTime();
+                day = day.AddSeconds(this.dt).ToLocalTime();
+                return day;
+            }
+
+        }
+
+    }
+}
